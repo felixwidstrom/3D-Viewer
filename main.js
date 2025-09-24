a62eec203fc9138c0e04d0206a8d0ce8a5f1510c
Felix Widström <felix.widstrom@hotmail.se>
2025-09-25 00:53:58 +0200

    Refactoring and basic functionality fixed.

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ import {OBJLoader} from 'https://threejsfundamentals.org/threejs/resources/three
 let scene, camera, renderer, object;
 let boundary = new THREE.Box3(), size = new THREE.Vector3(0,0,0), distance;
 
-Initialize();
-setTimeout(() => { Animate(); }, 500);
+initialize();
+setTimeout(() => { animate(); }, 500);
 
-function Initialize() {
+function initialize() {
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 1000);
@@ -18,7 +18,7 @@ function Initialize() {
     renderer.domElement.style.padding = "0";
     document.querySelector("main").appendChild(renderer.domElement);
 
-    let dirlight = new THREE.DirectionalLight(0xF1DAA4, 1, 100);
+    let dirlight = new THREE.DirectionalLight(0xFFFFFF, 1, 100);
     dirlight.castShadow = true;
     scene.add(dirlight);
 
@@ -26,8 +26,8 @@ function Initialize() {
     scene.add(amblight);    
 
     window.addEventListener('resize', onWindowResize);  
-    
-    LoadOBJ("models/template.obj");
+
+    loadOBJ("models/template.obj");
 }
 
 function onWindowResize() {
@@ -36,7 +36,7 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function LoadOBJ(path) {
+function loadOBJ(path) {
     if (object != undefined) {
         scene.remove(object);
     }
@@ -58,8 +58,8 @@ function LoadOBJ(path) {
     );
 }
 
-function Animate() {
-	requestAnimationFrame(Animate);
+function animate() {
+	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
 }
 
@@ -68,7 +68,7 @@ document.querySelector("#import").oninput = () => {
     let reader = new FileReader();
 
     reader.addEventListener("load", () => {
-        LoadOBJ(reader.result);
+        loadOBJ(reader.result);
     }, false);
 
     if (file) {
@@ -76,68 +76,114 @@ document.querySelector("#import").oninput = () => {
     }
 }
 
-let inputs = document.querySelectorAll("input");
-inputs[1].onchange = () => {
-    object.position.x = inputs[1].value;
-}
-inputs[2].onchange = () => {
-    object.position.y = inputs[2].value - size.y/2;
-}
-inputs[3].onchange = () => {
-    object.position.z = inputs[3].value;
-}
-inputs[4].onchange = () => {
-    object.rotation.x = (inputs[4].value)*Math.PI/180;
-}
-inputs[5].onchange = () => {
-    object.rotation.y = (inputs[5].value)*Math.PI/180;
-}
-inputs[6].onchange = () => {
-    object.rotation.z = (inputs[6].value)*Math.PI/180;
-}
-inputs[7].onchange = () => {
-    object.scale.x = inputs[7].value;
-}
-inputs[8].onchange = () => {
-    object.scale.y = inputs[8].value;
-}
-inputs[9].onchange = () => {
-    object.scale.z = inputs[9].value;
-}
-inputs[10].onchange = () => {
-    camera.fov = inputs[10].value;
-    camera.updateProjectionMatrix();
-}
-inputs[11].onchange = () => {
-    camera.far = inputs[11].value;
-    camera.updateProjectionMatrix();
-}
-inputs[12].onchange = () => {
-    camera.position.z = inputs[12].value + distance;
-}
-inputs[13].onclick = () => {
-    inputs[1].value = 0;
+const controls = {
+    movement: {
+        x: document.querySelector("input[name='move-x']"),
+        y: document.querySelector("input[name='move-y']"),
+        z: document.querySelector("input[name='move-z']")
+    },
+    rotation: {
+        x: document.querySelector("input[name='rotate-x']"),
+        y: document.querySelector("input[name='rotate-y']"),
+        z: document.querySelector("input[name='rotate-z']"),
+        xNumber: document.querySelector("input[name='rotate-x-number']"),
+        yNumber: document.querySelector("input[name='rotate-y-number']"),
+        zNumber: document.querySelector("input[name='rotate-z-number']")
+    },
+    scale: {
+        x: document.querySelector("input[name='scale-x']"),
+        y: document.querySelector("input[name='scale-y']"),
+        z: document.querySelector("input[name='scale-z']")
+    },
+    camera: {
+        fov: document.querySelector("input[name='camera-fov']"),
+        cutoff: document.querySelector("input[name='camera-cutoff']"),
+        position: document.querySelector("input[name='camera-position']")
+    },
+    reset: document.querySelector("input[name='reset']")
+};
+
+controls.reset.onclick = () => {
+    controls.movement.x.value = 0;
     object.position.x = 0;
-    inputs[2].value = 0;
+    controls.movement.y.value = 0;
     object.position.y = (-size.y)/2;
-    inputs[3].value = 0;
+    controls.movement.z.value = 0;
     object.position.z = 0;
-    inputs[4].value = 0;
+    controls.rotation.x.value = 0;
+    controls.rotation.xNumber.value = 0;
     object.rotation.x = 0;
-    inputs[5].value = 0;
+    controls.rotation.y.value = 0;
+    controls.rotation.yNumber.value = 0;
     object.rotation.y = 0;
-    inputs[6].value = 0;
+    controls.rotation.z.value = 0;
+    controls.rotation.zNumber.value = 0;
     object.rotation.z = 0;
-    inputs[7].value = 1;
+    controls.scale.x.value = 1;
     object.scale.x = 1;
-    inputs[8].value = 1;
+    controls.scale.y.value = 1;
     object.scale.y = 1;
-    inputs[9].value = 1;
+    controls.scale.z.value = 1;
     object.scale.z = 1;
-    inputs[10].value = 45;
+    controls.camera.fov.value = 45;
     camera.fov = 45;
-    inputs[11].value = 1000;
+    controls.camera.cutoff.value = 1000;
     camera.far = 1000;
+    controls.camera.position.value = 0;
     camera.position.z = distance;
     camera.updateProjectionMatrix();
+}
+
+controls.movement.x.oninput = () => {
+    object.position.x = controls.movement.x.value;
+}
+controls.movement.y.oninput = () => {
+    object.position.y = controls.movement.y.value - size.y/2;
+}
+controls.movement.z.oninput = () => {
+    object.position.z = controls.movement.z.value;
+}
+controls.rotation.x.oninput = () => {
+    object.rotation.x = (controls.rotation.x.value)*Math.PI/180;
+    controls.rotation.xNumber.value = controls.rotation.x.value;
+}
+controls.rotation.xNumber.oninput = () => {
+    object.rotation.x = (controls.rotation.xNumber.value)*Math.PI/180;
+    controls.rotation.x.value = controls.rotation.xNumber.value;
+}
+controls.rotation.y.oninput = () => {
+    object.rotation.y = (controls.rotation.y.value)*Math.PI/180;
+    controls.rotation.yNumber.value = controls.rotation.y.value;
+}
+controls.rotation.yNumber.oninput = () => {
+    object.rotation.y = (controls.rotation.yNumber.value)*Math.PI/180;
+    controls.rotation.y.value = controls.rotation.yNumber.value;
+}
+controls.rotation.z.oninput = () => {
+    object.rotation.z = (controls.rotation.z.value)*Math.PI/180;
+    controls.rotation.zNumber.value = controls.rotation.z.value;
+}
+controls.rotation.zNumber.oninput = () => {
+    object.rotation.z = (controls.rotation.zNumber.value)*Math.PI/180;
+    controls.rotation.z.value = controls.rotation.zNumber.value;
+}
+controls.scale.x.oninput = () => {
+    object.scale.x = controls.scale.x.value;
+}
+controls.scale.y.oninput = () => {
+    object.scale.y = controls.scale.y.value;
+}
+controls.scale.z.oninput = () => {
+    object.scale.z = controls.scale.z.value;
+}
+controls.camera.fov.oninput = () => {
+    camera.fov = controls.camera.fov.value;
+    camera.updateProjectionMatrix();
+}
+controls.camera.cutoff.oninput = () => {
+    camera.far = controls.camera.cutoff.value;
+    camera.updateProjectionMatrix();
+}
+controls.camera.position.oninput = () => {
+    camera.position.z = controls.camera.position.value + distance;
 }
